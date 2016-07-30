8b8eb98ada15add7a3b17fc49641ca12fd0d9e56
Tadatoshi Tokutake <tadatoshi@debian.profit-samurai.com>
2016-07-30 16:00:13 +0900

    [Feature] Comment out processes depending on Guest Additions

--- debian_bootstrap.sh
+++ debian_bootstrap.sh
@@ -5,12 +5,15 @@ USER_NAME=tadatoshi
 apt update
 apt upgrade -y
 
-### For VBox Guest Addition ###
+### For VBox Guest Additions ###
 apt install -y build-essential module-assistant
-m-a prepare
-# After inserting the disc image #
-# # cd /media/cd                 #
-# # bash VBoxLinuxAdditions.run  #
+m-a prepare -y
+# ### Install VBox Guest Addisions ###               #
+# cd /media/cd                                       #
+# bash VBoxLinuxAdditions.run                        #
+# ### Set shared directory ###                       #
+# gpasswd -a $USER_NAME vboxsf                       #
+# su - $USER_NAME -c 'ln -s /media/sf_share ~/share' #
 
 ### Enable for $USER_NAME to use "sudo" command ###
 SUDOERS_FILE=/etc/sudoers
@@ -22,10 +25,6 @@ chmod 440 $SUDOERS_FILE
 GRUB_CONFIG_FILE=/etc/default/grub
 sed -re 's/^GRUB_TIMEOUT=[0-9]+$/GRUB_TIMEOUT=0/' $GRUB_CONFIG_FILE -i
 update-grub
-
-### Set shared directory ###
-gpasswd -a $USER_NAME vboxsf
-su - $USER_NAME -c 'ln -s /media/sf_share ~/share'
 
 ### Install essential packages ###
 apt install -y vim git tree bash-completion terminator
