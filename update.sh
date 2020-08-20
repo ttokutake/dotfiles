6fd206a1cb303561f9d23e2f32379b8098a05fd3
ttokutake <tadatoshi.tokutake@gmail.com>
2020-08-20 10:23:01 +0900

    Set line break on stdin

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
 
 asdf plugin update --all
