d8381c7caa1717d7619e0b66db39743ebb40ac61
ttokutake <tadatoshi.tokutake@gmail.com>
2020-08-25 10:49:02 +0900

    Update AWS CLI

--- update.sh
+++ update.sh
@@ -7,3 +7,8 @@ brew update
 asdf plugin update --all
 
 pip3 install docker-compose --upgrade
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+rm -rf aws awscliv2.zip
