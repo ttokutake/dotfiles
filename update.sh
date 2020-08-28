fca3d17b5bb0f2fbefdb1c7c72acd477863f3981
ttokutake <tadatoshi.tokutake@gmail.com>
2020-08-28 20:29:58 +0900

    Remove garbage packages

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
+sudo apt autoremove
+sudo apt autoclean
 
 echo '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
