968580bdc108d11bf0a60218d1869bbb8924db90
ttokutake <tadatoshi.tokutake@gmail.com>
2020-11-23 12:31:52 +0900

    Update update.sh for WSL2 Debian

--- update.sh
+++ update.sh
@@ -1,16 +1,9 @@
+# System
 sudo apt update
-sudo apt upgrade -y
+sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean
 
-echo '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew update
-
+# asdf
+asdf update
 asdf plugin update --all
-
-pip3 install docker-compose --upgrade
-
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
-rm -rf aws awscliv2.zip
